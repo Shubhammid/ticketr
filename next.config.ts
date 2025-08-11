2bc67a1afa68f3cee6e1e8a6c3f4ca38335f08e3
Shubham Midgule <shubham.midgule17@gmail.com>
2025-08-11 20:06:59 +0530

    Builded Event Form Component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ images: {
+    remotePatterns: [
+      { hostname: "warmhearted-gnat-562.convex.cloud", protocol: "https" },
+      { hostname: "wary-anaconda-29.convex.cloud", protocol: "https" },
+    ],
+  },
 };
 
 export default nextConfig;
