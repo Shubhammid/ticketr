4ede1d4d205583ce4d1748e8f9027e6a86ea6a55
Shubham Midgule <shubham.midgule17@gmail.com>
2025-08-12 18:22:41 +0530

    Edit View Functionality is Done

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
  images: {
     remotePatterns: [
-      { hostname: "warmhearted-gnat-562.convex.cloudc", protocol: "https" },
+      { hostname: "warmhearted-gnat-562.convex.cloud", protocol: "https" },
       { hostname: "wary-anaconda-29.convex.cloud", protocol: "https" },
     ],
   },
